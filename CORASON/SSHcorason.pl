f7c40aa1631240ae0570bd05dc162ea0a35a5a2e
nselem35 <nselem84@gmail.com>
2017-09-12 11:36:29 -0500

    antiSMASH to entry point

--- CORASON/SSHcorason.pl
+++ CORASON/SSHcorason.pl
@@ -4,6 +4,7 @@ use strict;
 my $yourquery=$ARGV[0]; 
 my $yourRAST=$ARGV[1]; 
 my $yourspecial=$ARGV[2];
+my $antiSMASH=$ARGV[3];
 
-print "corason.pl -q $yourquery -rast_ids $yourRAST -s $yourspecial\n";
-system "corason.pl -q $yourquery -rast_ids $yourRAST -s $yourspecial\n";
+print "corason.pl -q $yourquery -rast_ids $yourRAST -s $yourspecial -a $antiSMASH\n";
+system "corason.pl -q $yourquery -rast_ids $yourRAST -s $yourspecial -a $antiSMASH\n";
